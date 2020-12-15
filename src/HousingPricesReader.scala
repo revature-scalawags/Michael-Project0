04b72f7701e374cff7fa4fecfabd67e2ab9cf335
MichaelT950 <michaeltsoumpariotis@gmail.com>
2020-12-15 13:32:31 -0600

    ADDED findMax() FUNCTION

--- src/HousingPricesReader.scala
+++ src/HousingPricesReader.scala
@@ -8,6 +8,7 @@ object HousingPricesReader extends App{
   println("What file would you like to read?")
 
   val fileName = scala.io.StdIn.readLine()
+  val file = io.Source.fromFile(fileName)
   val helpInstructions = "HelpInstructions"
   val helpFile = io.Source.fromFile(helpInstructions)
 
@@ -21,20 +22,29 @@ object HousingPricesReader extends App{
   def readInst (f : BufferedSource) ={
     for (ln <- f.getLines()) println(ln)
   }
+
   def initState() = {
     println("What would you like to do?")
     val cmd = scala.io.StdIn.readLine()
     val init =  cmd match {
       case "1" => printHouses()
+      case "2" => findMax()
     }
   }
 
   def printHouses() = {
-    val file = io.Source.fromFile(fileName)
     for (ln <- file.getLines().drop(1)) {
       val Array(index, sqft, beds, baths, zip, year, price) = ln.split(",").map(_.trim)
-      println(s"#$index $sqft sqft $beds beds $baths baths $zip $year $price")
+      println(s"#$index ${sqft}sqft ${beds}beds ${baths}baths $zip $year ${price}")
     }
+  }
+  def findMax() = {
+    var max = 0
+    for (ln <- file.getLines().drop(1)){
+      val price = ln.split(",")(6).trim
+      if (max<=price.toInt) max = price.toInt
+    }
+    println(max)
   }
 
 }
