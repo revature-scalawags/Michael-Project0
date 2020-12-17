fcc361f04d8fa1545af138246d841f667a3d4f0e
MichaelT950 <michaeltsoumpariotis@gmail.com>
2020-12-17 16:39:33 -0600

    ADDED ADDITIONAL FUNCTIONALITY TO findMax AND findMin FUNCTIONS  - Now works with number of beds, baths, year, zip, and living space

--- src/HousingPricesReader.scala
+++ src/HousingPricesReader.scala
@@ -1,9 +1,9 @@
 import scala.io.BufferedSource
 
-/**HousingPricesReader
+/** HousingPricesReader
  *
  */
-object HousingPricesReader extends App{
+object HousingPricesReader extends App {
 
   println("What file would you like to read?")
 
@@ -13,13 +13,13 @@ object HousingPricesReader extends App{
   val helpFile = io.Source.fromFile(helpInstructions)
 
   //INITIATE APP
-  println("Here are a list of commands: " )
+  println("Here are a list of commands: ")
   readInst(helpFile)
   initState()
 
   //POST FUNCTIONS HERE
   //make a read instructions function
-  def readInst (f : BufferedSource) ={
+  def readInst(f: BufferedSource) = {
     for (ln <- f.getLines()) println(ln)
   }
 
@@ -28,36 +28,60 @@ object HousingPricesReader extends App{
     val cmd = scala.io.StdIn.readLine()
     do {
       cmd match {
-        case "1" => printHouses()
+        case "1" => parseHouses() //use this to parse data
         case "2" => findMax()
         case "3" => findMin()
-        case "4" => file.getLines() foreach println  //this prints houses
+        case "4" => printHouses() //this prints houses
         case "q" => print("Exiting")
         case _ => print("Invalid option")
       }
     }
-    while (scala.io.StdIn.readLine() != "q" )
+    while (scala.io.StdIn.readLine() != "q")
   }
 
   def printHouses() = {
+    file.getLines() foreach println
+  }
+
+  def parseHouses() = {
     for (ln <- file.getLines().drop(1)) {
       val Array(index, sqft, beds, baths, zip, year, price) = ln.split(",").map(_.trim)
       println(s"#$index ${sqft}sqft ${beds}beds ${baths}baths $zip $year ${price}")
     }
   }
+
   def findMax() = {
+    println("What do you want to find the max number of?" +
+      "\n1 - Living Space (sqft)" +
+      "\n2 - Beds" +
+      "\n3 - Baths" +
+      "\n4 - Zip" +
+      "\n5 - Year" +
+      "\n6- List Price")
+
+    val input =  scala.io.StdIn.readLine()
     var max = 0
-    for (ln <- file.getLines().drop(1)){
-      val price = ln.split(",")(6).trim
-      if (max<=price.toInt) max = price.toInt
+    for (ln <- file.getLines().drop(1)) {
+      val price = ln.split(",")(input.toInt).trim
+      if (max <= price.toInt) max = price.toInt
     }
     println(max)
   }
+
   def findMin() = {
+    println("What do you want to find the min number of?" +
+      "\n1 - Living Space (sqft)" +
+      "\n2 - Beds" +
+      "\n3 - Baths" +
+      "\n4 - Zip" +
+      "\n5 - Year" +
+      "\n6- List Price")
+
+    val input =  scala.io.StdIn.readLine()
     var min = Int.MaxValue
-    for (ln <- file.getLines().drop(1)){
-      val price = ln.split(",")(6).trim
-      if (min>=price.toInt) min = price.toInt
+    for (ln <- file.getLines().drop(1)) {
+      val price = ln.split(",")(input.toInt).trim
+      if (min >= price.toInt) min = price.toInt
     }
     println(min)
   }
