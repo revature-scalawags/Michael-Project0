b82ed039eb4fa5a5622ffb25fae3bc9a4d526465
MichaelT950 <michaeltsoumpariotis@gmail.com>
2020-12-20 00:42:06 -0600

    -REFACTORED CODE -ADDED avgPrice() METHOD

--- src/House.scala
+++ src/House.scala
@@ -1,6 +1,2 @@
 case class House(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String) {}
 
-//object House{
-//  def apply(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String): House =
-//    House(index, sqft, beds, baths, zip, year, price)
-//}
--- src/HousingPricesReader.scala
+++ src/HousingPricesReader.scala
@@ -1,5 +1,7 @@
+import java.io.FileNotFoundException
 import scala.collection.mutable.ArrayBuffer
 import scala.io.BufferedSource
+import scala.collection.mutable.Map
 
 /** HousingPricesReader
  *
@@ -12,42 +14,29 @@ object HousingPricesReader extends App {
 
   //INITIATE APP
   val file = readFile()
-  readInst(helpFile)
-  initState(file)
+  val housesArrayBuffer = parseHouses()
+  //instructions
+  instructions
+  initState()
 
   //POST FUNCTIONS HERE
-  //make a read instructions function
+  //IO, FILE HANDLING
   def readInst(f: BufferedSource) = {
     println("Here are a list of commands: ")
-    for (ln <- f.getLines()) println(ln)
+    try {
+      for (ln <- f.getLines()) println(ln)
+    } catch {
+      case e: FileNotFoundException => println("File not found")
+    } finally {
+      f.close()
+    }
+    //for (ln <- f.getLines()) println(ln)
   }
+
   def readFile(): BufferedSource = {
     println("What file would you like to read?")
     fileName = scala.io.StdIn.readLine()
     return io.Source.fromFile(fileName)
-  }
-
-  def initState(f: BufferedSource)= {
-    var cmd = ""
-    while (cmd != "q") {
-      println("What would you like to do?")
-      cmd = scala.io.StdIn.readLine()
-      cmd match {
-        case "1" => parseHouses() //use this to parse data
-        case "2" => findMax()
-        case "3" => findMin()
-        case "4" => printHouses(f)//this prints houses
-        case "q" => println("Exiting")
-        case _ => println("Invalid option")
-      }
-    }
-
-  }
-
-  def printHouses(f: BufferedSource) = {
-    println("entered printHouses()")
-    f.getLines() foreach println
-    println(f.length)
   }
 
   def parseHouses() = {
@@ -57,7 +46,78 @@ object HousingPricesReader extends App {
       var house = House(s"$index", s"$sqft", s"$beds", s"$baths", s"$zip", s"$year", s"$price")
       housesArrayBuffer.addOne(house)
     }
+    housesArrayBuffer
+  }
+
+  def avgPrice(): Int = {
+    var avg = 0
+    var m = Map[String, Int]()
+
+    for(a <- 0 until housesArrayBuffer.length){
+      m.update(housesArrayBuffer(a).index, housesArrayBuffer(a).price.toInt)
+    }
+    m.foreach(x => avg += x._2)
+    avg/housesArrayBuffer.length
+  }
+
+  //FUNCTIONALITY
+  def initState() = {
+    var cmd = ""
+    while (cmd != "q") {
+      println("What would you like to do?")
+      try {
+        cmd = scala.io.StdIn.readLine()
+        cmd match {
+          case "1" => printHouses() //this prints houses
+          case "2" => println("Max price of a house is $" + maxPrice())
+          case "3" => println("Min price of a house is $" + minPrice())
+          case "4" => println("Average price is $"+ avgPrice())
+          case "5" => instructions
+          case "q" => println("Exiting")
+          case _ => println("Invalid option")
+        }
+      } catch {
+        case e: FileNotFoundException => println("File not found")
+      } finally {
+        file.close()
+      }
+
+    }
+
+  }
+
+  def printHouses() = {
+    println("House(index, sqft, beds, baths, zip, year, price)")
     housesArrayBuffer foreach println
+  }
+
+  def maxPrice(): Int = {
+    var max = 0
+    for (h <- housesArrayBuffer) {
+      val price = h.price
+      if (max <= price.toInt) max = price.toInt
+    }
+    max
+  }
+
+  def minPrice(): Int = {
+    var min = Int.MaxValue
+    for (h <- housesArrayBuffer) {
+      val price = h.price
+      if (min >= price.toInt) min = price.toInt
+    }
+    min
+  }
+
+  def instructions = {
+    println("Here is a list of commands: " +
+      "\n1 - Print houses" +
+      "\n2 - Find max price of house" +
+      "\n3 - Find min price of house" +
+      "\n4 - Find average price"+
+      "\n5 - Instructions" +
+      "\nq - Exit")
+
   }
 
   def findMax() = {
@@ -70,7 +130,7 @@ object HousingPricesReader extends App {
       "\n5 - Year" +
       "\n6- List Price")
 
-    val input =  scala.io.StdIn.readLine()
+    val input = scala.io.StdIn.readLine()
     var max = 0
     for (ln <- file.getLines().drop(1)) {
       val price = ln.split(",")(input.toInt).trim
@@ -88,7 +148,7 @@ object HousingPricesReader extends App {
       "\n5 - Year" +
       "\n6- List Price")
 
-    val input =  scala.io.StdIn.readLine()
+    val input = scala.io.StdIn.readLine()
     var min = Int.MaxValue
     for (ln <- file.getLines().drop(1)) {
       val price = ln.split(",")(input.toInt).trim
