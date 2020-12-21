9cb6bad31211c65dd57f443617ab07ad18d598ba
MichaelT950 <michaeltsoumpariotis@gmail.com>
2020-12-21 13:28:12 -0600

    -REFACTORED CODE -ADDED outputToFile -ADDED findUndervalued -ADDED printHouses(ArrayBuffer[House]) -ADDED findUndervalued

--- src/House.scala
+++ src/House.scala
@@ -1,2 +1,6 @@
-case class House(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String) {}
+case class House(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String) {
+  override def toString: String = {
+    return s"$index, $sqft, $beds, $baths, $zip, $year, $price"
+  }
+}
 
--- src/HousingPricesReader.scala
+++ src/HousingPricesReader.scala
@@ -1,10 +1,12 @@
+import HousingPricesReader.instructions
+
+import java.io._
 import java.io.FileNotFoundException
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer, Map}
 import scala.io.BufferedSource
-import scala.collection.mutable.Map
 
 /** HousingPricesReader
- *
+ * AUTHOR: Michael Tsoumpariotis
  */
 object HousingPricesReader extends App {
 
@@ -17,21 +19,10 @@ object HousingPricesReader extends App {
   val housesArrayBuffer = parseHouses()
   //instructions
   instructions
-  initState()
+  cli
 
   //POST FUNCTIONS HERE
   //IO, FILE HANDLING
-  def readInst(f: BufferedSource) = {
-    println("Here are a list of commands: ")
-    try {
-      for (ln <- f.getLines()) println(ln)
-    } catch {
-      case e: FileNotFoundException => println("File not found")
-    } finally {
-      f.close()
-    }
-    //for (ln <- f.getLines()) println(ln)
-  }
 
   def readFile(): BufferedSource = {
     println("What file would you like to read?")
@@ -52,42 +43,63 @@ object HousingPricesReader extends App {
   def avgPrice(): Int = {
     var avg = 0
     var m = Map[String, Int]()
-
-    for(a <- 0 until housesArrayBuffer.length){
+    for (a <- 0 until housesArrayBuffer.length) {
       m.update(housesArrayBuffer(a).index, housesArrayBuffer(a).price.toInt)
     }
     m.foreach(x => avg += x._2)
-    avg/housesArrayBuffer.length
+    avg / housesArrayBuffer.length
+  }
+
+  def outputToFile(name: String, argArrayBuffer: ArrayBuffer[House]) = {
+    val file = new File(name)
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write("Index, Living Space (sq ft), Beds, Baths, Zip, Year, List Price ($)\n")
+    argArrayBuffer.foreach(House => bw.write(House.toString+"\n"))
+    bw.close()
+    println("Done")
+  }
+
+  def addHouse(sqft: String, beds: String, baths: String, zip: String, year: String, price: String): Unit = {
+    val index = (housesArrayBuffer.length+1).toString
+    housesArrayBuffer += new House(index, sqft, beds, baths, zip, year, price)
+  }
+
+  def findUndervalued(housesArrayBuffer: ArrayBuffer[House]):ArrayBuffer[House]={
+    val bestValueHouses = housesArrayBuffer.filter(House => (House.price.toInt < avgPrice()))
+    bestValueHouses
+  }
+
+  def printHouses(hab: ArrayBuffer[House]) = {
+    println("#, sqft, beds, baths, zip, year, price")
+    hab foreach println
   }
 
   //FUNCTIONALITY
-  def initState() = {
+  def cli() = {
     var cmd = ""
+    val formatter = java.text.NumberFormat.getIntegerInstance
     while (cmd != "q") {
       println("What would you like to do?")
-      try {
-        cmd = scala.io.StdIn.readLine()
-        cmd match {
-          case "1" => printHouses() //this prints houses
-          case "2" => println("Max price of a house is $" + maxPrice())
-          case "3" => println("Min price of a house is $" + minPrice())
-          case "4" => println("Average price is $"+ avgPrice())
-          case "5" => instructions
-          case "q" => println("Exiting")
-          case _ => println("Invalid option")
-        }
-      } catch {
-        case e: FileNotFoundException => println("File not found")
-      } finally {
-        file.close()
+      cmd = scala.io.StdIn.readLine()
+      cmd match {
+        case "print" => printHouses() //this prints houses
+        case s"add $sqft $beds $baths $zip $year $price" => addHouse(sqft, beds, baths, zip, year, price)
+        case "max price" => val s = "Max price of a house is $" + "%,d".format(maxPrice()); println(s)
+        case "min price" => val s = "Min price of a house is $" + "%,d".format(minPrice()); println(s)
+        case "average" => val s = "Average price is $" + "%,d".format(avgPrice()); println(s)
+        case "find under" => printHouses(findUndervalued(housesArrayBuffer))
+        case s"save" => outputToFile(fileName, housesArrayBuffer)
+        case s"save as $name" => outputToFile(name, housesArrayBuffer)
+        case s"save undervalued houses as $bVName" => outputToFile(bVName, findUndervalued(housesArrayBuffer))
+        case "i"|"?"|"help" => instructions
+        case ("q"|"exit") => println("Exiting");
+        case _ => println("Invalid option. Press i for list of instructions")
       }
-
     }
-
   }
 
   def printHouses() = {
-    println("House(index, sqft, beds, baths, zip, year, price)")
+    println("#, sqft, beds, baths, zip, year, price")
     housesArrayBuffer foreach println
   }
 
@@ -111,50 +123,16 @@ object HousingPricesReader extends App {
 
   def instructions = {
     println("Here is a list of commands: " +
-      "\n1 - Print houses" +
-      "\n2 - Find max price of house" +
-      "\n3 - Find min price of house" +
-      "\n4 - Find average price"+
-      "\n5 - Instructions" +
-      "\nq - Exit")
+      "\nprint     - Print houses" +
+      "\nadd       - Add house with specified sqft, beds, baths, zip, year, and price"+
+      "\nmax price - Find max price of house" +
+      "\nmin price - Find min price of house" +
+      "\naverage   - Find average price" +
+      "\nfind under- Find houses that are undervalued"+
+      "\nsave      - Save current file" +
+      "\nsave as   - Save current file as <specifiedName>" +
+      "\ni         - Instructions" +
+      "\nq         - exit")
 
   }
-
-  def findMax() = {
-    println("entered findMax()")
-    println("What do you want to find the max number of?" +
-      "\n1 - Living Space (sqft)" +
-      "\n2 - Beds" +
-      "\n3 - Baths" +
-      "\n4 - Zip" +
-      "\n5 - Year" +
-      "\n6- List Price")
-
-    val input = scala.io.StdIn.readLine()
-    var max = 0
-    for (ln <- file.getLines().drop(1)) {
-      val price = ln.split(",")(input.toInt).trim
-      if (max <= price.toInt) max = price.toInt
-    }
-    println(max)
-  }
-
-  def findMin() = {
-    println("What do you want to find the min number of?" +
-      "\n1 - Living Space (sqft)" +
-      "\n2 - Beds" +
-      "\n3 - Baths" +
-      "\n4 - Zip" +
-      "\n5 - Year" +
-      "\n6- List Price")
-
-    val input = scala.io.StdIn.readLine()
-    var min = Int.MaxValue
-    for (ln <- file.getLines().drop(1)) {
-      val price = ln.split(",")(input.toInt).trim
-      if (min >= price.toInt) min = price.toInt
-    }
-    println(min)
-  }
-
 }
