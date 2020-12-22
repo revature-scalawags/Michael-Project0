1ac88419f25e112944486127fe0f3986345ad56a
MichaelT950 <michaeltsoumpariotis@gmail.com>
2020-12-22 16:38:46 -0600

    ADDED mongodb FUNCTIONALITY

--- src/main/scala/House.scala
+++ src/main/scala/House.scala
@@ -1,11 +1,18 @@
-case class House(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String) {
+import org.bson.types.ObjectId
+
+case class House(_id: ObjectId,index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String) {
+
+//  def apply(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String ): House =
+//    House(new ObjectId(), index, sqft, beds, baths, zip, year, price)
+
   override def toString: String = {
     return s"$index, $sqft, $beds, $baths, $zip, $year, $price"
   }
 }
 
-//object House {
-//  def apply(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String): House =
-//    House(index, sqft, beds, baths, zip, year, price)
-//}
+
+object House {
+  def apply(index: String, sqft: String, beds: String, baths: String, zip: String, year: String, price: String ): House =
+    House(new ObjectId(), index, sqft, beds, baths, zip, year, price)
+}
 
